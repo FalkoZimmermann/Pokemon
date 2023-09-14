77ffa8526c42fd42561f77fa41e6171fc4423bcd
FalkoZimmermann <falko.zimmermann@proton.me>
2023-09-14 12:50:02 +0200

    tre

--- script.js
+++
@@ -1,7 +0,0 @@
-async function init() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
-    let response = await(url);
-    let responseAsJson = await response.json();
-
-    console.log('loaded pokemon', init);
-}
